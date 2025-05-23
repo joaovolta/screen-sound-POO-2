037b8c6431567d9faf67d518df2f908e95d25662
joaovolta <joaovolta04@gmail.com>
2025-05-23 17:37:22 -0300

    Adicao da class MenuAvaliarBandas e uso de herança

--- Menus/MenuExibirDetalhes.cs
+++ Menus/MenuExibirDetalhes.cs
@@ -2,17 +2,8 @@
 
 namespace ScreenSound.Menus;
 
-internal class MenuExibirDetalhes
+internal class MenuExibirDetalhes : Menu
 {
-    public void ExibirTituloDaOpcao(string titulo)
-    {
-        int quantidadeDeLetras = titulo.Length;
-        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-        Console.WriteLine(asteriscos);
-        Console.WriteLine(titulo);
-        Console.WriteLine(asteriscos + "\n");
-    }
-
     public void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
         Console.Clear();
